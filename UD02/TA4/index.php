577b78db71c5dc11242501cde17596f722286a2b
fjpaneda <fjpaneda@birt.eus>
2024-11-11 13:54:15 +0100

    estructura

---
+++ UD02/TA4/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla de colores de resistencias</title>
+</head>
+<body>
+    <h1>Tabla de colores de resistencias</h1>
+    <p>Estos son los valores correspondientes a las bandas de colores de las resistencias:</p>
+    
+</body>
+</html>
---
+++ UD02/TA4/resistor-bands.php
@@ -0,0 +1,12 @@
+<?php
+    $colores = array( 0 => "negro",
+        1 => "marrón",
+        2 => "rojo",
+        3 => "naranja",
+        4 => "amarillo",
+        5 => "verde",
+        6 => "azul",
+        7 => "morado",
+        8 => "gris",
+        9 => "blanco");
+?>
